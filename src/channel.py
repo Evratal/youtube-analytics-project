fac08ae5e8349c41b1d4f64789c3e3a289043bca
Evgeniy Ratuev <evratal@yandex.ru>
2023-09-17 00:17:07 +0400

    Complete work

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+from googleapiclient.discovery import build
+
+from helper.youtube_api_manual import youtube
 
 
 class Channel:
@@ -5,8 +8,10 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        self.channel_id = channel_id
         pass
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        request = youtube.search().list(part="snippet", channelId=self.channel_id, maxResults=10, type="video")
+        response = request.execute()
+        return response
